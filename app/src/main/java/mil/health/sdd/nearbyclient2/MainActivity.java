88b8e17c319ba5896b753de2570b0ad812fdcd26
Jack Lightfoot <jack.lightfoot@tee2.org>
2018-06-13 13:29:40 -0700

    more files

---
+++ app/src/main/java/mil/health/sdd/nearbyclient2/MainActivity.java
@@ -0,0 +1,24 @@
+package mil.health.sdd.nearbyclient2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    public static final String EXTRA_ADVERTISE_MESSAGE = "mil.health.sdd.nearbyclient2.ADVERTISE_MESSAGE";
+    public static final String EXTRA_ADVERTISE_MESSAGE_STRING = "user_button";
+    public static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Advertise button */
+    public void advertiseCA(View view) {
+        Intent intent = new Intent(this, AdvertiseCAActivity.class);
+        intent.putExtra(EXTRA_ADVERTISE_MESSAGE, EXTRA_ADVERTISE_MESSAGE_STRING);
+        startActivity(intent);
+    }
+}
